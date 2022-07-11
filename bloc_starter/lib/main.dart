a89ad07c4f6a66712eebc53448547b47ac5942d3
wahyuindra <wahyuindra712@gmail.com>
2022-07-11 23:19:17 +0700

    change page

--- bloc_starter/lib/main.dart
+++ bloc_starter/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_starter/views/home_page.dart';
+import 'package:bloc_starter/views/stream_concept_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: StreamConceptPage(),
     );
   }
 }
