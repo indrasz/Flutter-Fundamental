83e9522bf26c59c4818d6c00d2ff9ae3c67c506c
wahyuindra <wahyuindra712@gmail.com>
2022-07-11 22:10:38 +0700

    add stream builder

--- bloc_starter/lib/views/home_page.dart
+++ bloc_starter/lib/views/home_page.dart
@@ -3,8 +3,47 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Stream<int> countStream() async* {
+    for(int i = 1; i <= 10; i++ ){
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  } 
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    print("Rebuild");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: const Text("Stream Apps"),
+        )
+      ),
+
+      body: StreamBuilder(
+        stream: countStream(),
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(
+              child: Text(
+                "Loading...", 
+                style: TextStyle(
+                  fontSize: 50
+                ),
+              ),
+            );
+          } else {
+            return Center(
+              child: Text(
+                "${snapshot.data}", 
+                style: const TextStyle(
+                  fontSize: 50
+                ),
+              ),
+            );
+          }
+        }
+      ),
+    );
   }
 }
